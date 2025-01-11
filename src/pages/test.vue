bacc46747a95c26c480dedec5a02494d1f27f83c
dangnguyenblackie <phucdang91thcsdl@gmail.com>
2025-01-11 17:18:48 +0700

    #8.9: Add transition for first loading map

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -12,14 +12,33 @@ export default {
         const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
         ctx.fillRect(0, 0, canvas.width, canvas.height)
 
+        let x = 0;
+        let y = -1000;
+
+        const firstX = -3100;
+        const firstY = -1000;
+        
         const img = new Image();
         img.src = 'rpg-assets/Images/main-map.png';
         img.onload = () => {
-          ctx.drawImage(img, 0 ,0);
+            const animate = () => {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                ctx.drawImage(img, x, y);
+
+                if (x > firstX)
+                {
+                    x -= Math.min(20, x - firstX);
+                }
+
+                if (x > firstX)
+                {
+                    requestAnimationFrame(animate);
+                } 
+            };
+            animate();
         }
     },
   };
-
 </script>
 
 <style scoped>
