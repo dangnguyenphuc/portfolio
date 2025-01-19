eea1e7a949de37adef9fa06cffd07132cdae2cea
dangnguyenblackie <phucdang91thcsdl@gmail.com>
2025-01-19 18:13:22 +0700

    #8.13: Enable user to move

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -24,23 +24,20 @@ class Coordinate2D {
   }
 
   // Method to add another coordinate to this one
-  add(coord: Coordinate2D): Coordinate2D {
-    return new Coordinate2D(this.x + coord.x, this.y + coord.y);
+  add(coord: Coordinate2D) {
+    this.x += coord.x;
+    this.y += coord.y;
   }
 
   // Method to subtract another coordinate from this one
-  subtract(coord: Coordinate2D): Coordinate2D {
-    return new Coordinate2D(this.x - coord.x, this.y - coord.y);
+  subtract(coord: Coordinate2D) {
+    this.x -= coord.x;
+    this.y -= coord.y;
   }
 
   // Method to calculate distance to another coordinate
   distanceTo(coord: Coordinate2D): number {
     return Math.sqrt(Math.pow(this.x - coord.x, 2) + Math.pow(this.y - coord.y, 2));
-  }
-
-  // String representation of the coordinate
-  toString(): string {
-    return `(${this.x}, ${this.y})`;
   }
 }
 
@@ -76,9 +73,136 @@ class Sprite {
         this.image = image
     }
 
+    draw(ctx: CanvasRenderingContext2D, offset: Coordinate2D = new Coordinate2D(0,0))
+    {
+        ctx.drawImage(this.image, this.position.x + offset.x, this.position.y + offset.y);
+    }
+}
+
+class Map {
+    mapSprite: Sprite;
+    offset: Coordinate2D;
+
+    constructor(position: Coordinate2D, image: HTMLImageElement, offset: Coordinate2D = new Coordinate2D(0,0)) {
+        this.mapSprite = new Sprite(position, image);
+        this.offset = offset;
+    }
+
     draw(ctx: CanvasRenderingContext2D)
     {
-        ctx.drawImage(this.image, this.position.x, this.position.y);
+        this.mapSprite.draw(ctx, this.offset);
+    }
+
+    updateOffset(offset: Coordinate2D = new Coordinate2D(0,0))
+    {
+        this.offset.add(offset);
+    }
+}
+
+class Player {
+    lastKey: string = '';
+    keys = {
+        w: {
+            pressed: false,
+        },
+        a: {
+            pressed: false,
+        },
+        s: {
+            pressed: false,
+        },
+        d: {
+            pressed: false,
+        },
+    };
+    sprite: Sprite;
+    
+    constructor(position: Coordinate2D, image: HTMLImageElement)
+    {
+        this.sprite = new Sprite(position, image);
+
+        window.addEventListener("keydown", (e) => {
+            switch(e.key)
+            {
+                case 'w':
+                    this.keys.w.pressed = true;
+                    this.keys.a.pressed = false;
+                    this.keys.s.pressed = false;
+                    this.keys.d.pressed = false;
+                    this.lastKey = 'w';
+                    break;
+                case 's':
+                    this.keys.w.pressed = false;
+                    this.keys.a.pressed = false;
+                    this.keys.s.pressed = true;
+                    this.keys.d.pressed = false;
+                    this.lastKey = 's';
+                    break
+                case 'd':
+                    this.keys.w.pressed = false;
+                    this.keys.a.pressed = false;
+                    this.keys.s.pressed = false;
+                    this.keys.d.pressed = true;
+                    this.lastKey = 'd';
+                    break;
+                case 'a':
+                    this.keys.w.pressed = false;
+                    this.keys.a.pressed = true;
+                    this.keys.s.pressed = false;
+                    this.keys.d.pressed = false;
+                    this.lastKey = 'a';
+                    break
+                default:
+                    break;
+                
+            }
+        });
+
+        window.addEventListener("keyup", (e) => {
+            switch(e.key)
+            {
+                case 'w':
+                    this.keys.w.pressed = false;
+                    break;
+                case 's':
+                    this.keys.s.pressed = false;
+                    break
+                case 'd':
+                    this.keys.d.pressed = false;
+                    break;
+                case 'a':
+                    this.keys.a.pressed = false;
+                    break
+                default:
+                    break;
+            }
+        });
+    }
+
+    draw(ctx: CanvasRenderingContext2D)
+    {
+        this.control()
+        this.sprite.draw(ctx)
+    }
+
+    control()
+    {
+        if (this.keys.w.pressed && this.lastKey == 'w')
+        {
+            this.sprite.position.y -= 3;
+        }   
+        else if (this.keys.a.pressed && this.lastKey == 'a')
+        {
+            this.sprite.position.x -= 3;
+        }   
+        else if (this.keys.s.pressed && this.lastKey == 's')
+        {
+            this.sprite.position.y += 3;
+        }   
+        else if (this.keys.d.pressed && this.lastKey == 'd')
+        {
+            this.sprite.position.x += 3;
+        }   
     }
 }
 
@@ -101,131 +225,39 @@ export default {
                 ))
             }
         });
-
-        console.log(boundaries);
         const canvas = document.getElementById('myCanvas') as HTMLCanvasElement;
         const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
         ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-        let backgroundX = 0;
-        let backgroundY = -1000;
-
         const img = new Image();
         img.src = 'rpg-assets/Images/main-map.png';
 
-        const keys = {
-            w : {
-              pressed: false  
-            },
-            s : {
-              pressed: false  
-            },
-            d : {
-              pressed: false  
-            },
-            a : {
-              pressed: false  
-            }
-        }
         
-        const background = new Sprite(new Coordinate2D(-3100, -950), img);
         const playerImg = new Image();
         playerImg.src = '/rpg-assets/Images/playerDown.png';
-        let lastKey = '';
+        
+        let mainMap = new Map(new Coordinate2D(-3100, -950), img);
+        let player = new Player(new Coordinate2D(canvas.width/2 - playerImg.width/2, canvas.height/2 - playerImg.height/2), playerImg);
 
         function animate()
         {
             window.requestAnimationFrame(animate);
-            background.draw(ctx);
-            ctx.drawImage(playerImg, 
-                0,
-                0,
-                playerImg.width/4,
-                playerImg.height,
-                canvas.width/2 - playerImg.width/2, 
-                canvas.height/2 - playerImg.height/2,
-                playerImg.width/4,
-                playerImg.height
-            );
-
-            if (keys.w.pressed && lastKey == 'w')
-            {
-                
-            }   
-            else if (keys.a.pressed && lastKey == 'a')
-            {
-                
-            }   
-            else if (keys.s.pressed && lastKey == 's')
-            {
-                
-            }   
-            else if (keys.d.pressed && lastKey == 'd')
-            {
-                
-            }   
+            mainMap.draw(ctx);
+            player.draw(ctx);
+            // ctx.drawImage(playerImg, 
+            //     0,
+            //     0,
+            //     playerImg.width/4,
+            //     playerImg.height,
+            //     canvas.width/2 - playerImg.width/2, 
+            //     canvas.height/2 - playerImg.height/2,
+            //     playerImg.width/4,
+            //     playerImg.height
+            // );
             
         }
 
         animate();
-
-        window.addEventListener("keydown", (e) => {
-            switch(e.key)
-            {
-                case 'w':
-                    keys.w.pressed = true;
-                    keys.a.pressed = false;
-                    keys.s.pressed = false;
-                    keys.d.pressed = false;
-                    lastKey = 'w';
-                    break;
-                case 's':
-                    keys.w.pressed = false;
-                    keys.a.pressed = false;
-                    keys.s.pressed = true;
-                    keys.d.pressed = false;
-                    lastKey = 's';
-                    break
-                case 'd':
-                    keys.w.pressed = false;
-                    keys.a.pressed = false;
-                    keys.s.pressed = false;
-                    keys.d.pressed = true;
-                    lastKey = 'd';
-                    break;
-                case 'a':
-                    keys.w.pressed = false;
-                    keys.a.pressed = true;
-                    keys.s.pressed = false;
-                    keys.d.pressed = false;
-                    lastKey = 'a';
-                    break
-                default:
-                    break;
-                
-            }
-        });
-
-        window.addEventListener("keyup", (e) => {
-            switch(e.key)
-            {
-                case 'w':
-                    keys.w.pressed = false;
-                    break;
-                case 's':
-                    keys.s.pressed = false;
-                    break
-                case 'd':
-                    keys.d.pressed = false;
-                    break;
-                case 'a':
-                    keys.a.pressed = false;
-                    break
-                default:
-                    break;
-                
-            }
-        });
     },
   };
 </script>
