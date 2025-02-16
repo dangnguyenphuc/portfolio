132f9c15d4a1b858d7f81605aebb72eb51abb043
dangnguyenblackie <phucdang91thcsdl@gmail.com>
2025-02-16 15:08:31 +0700

    #3: Added color when change theme on header navbar

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,64 +1,93 @@
 <template>
     
     <v-row class="header-bar d-flex pa-0 my-0 align-center justify-md-start justify-end" style="outline: 1px solid red;">
-      <v-col class="theme-change-col d-flex pa-0 justify-md-start justify-end" style="outline: 1px solid red;">
-        <v-btn @click="toggleTheme" class="theme-btn square-btn bg-yellow d-flex align-center justify-center pa-0">
-            <v-icon class="ml-2 ">{{ icon }}</v-icon>
-        </v-btn>
-      </v-col>
 
-      <v-col class="d-none d-md-flex">
-        <div class="d-flex flex-column ml-2 ga-2">
+      <v-col class="d-none d-md-flex pa-0">
+        <div class="d-flex flex-column ml-10">
           <v-row>
-            <div class="border-sm border-opacity-100 bg-primary text-body-1">MENU</div>
+            <div class="border-sm border-opacity-100 bg-primary text-caption menu-text pa-1">MENU</div>
           </v-row>
           <v-row class="ga-10 ml-2">
-              <div  v-for="(item, i) in menuItems" :key="i" id="" class="elevation-0 pa-0 text-body-2" @click="test">
-                  <div class="slide-bg">{{ item }}</div>
+              <div v-for="(item) in menuItems" :key="item.id" class="elevation-0 pa-0 text-subtitle-1" @click="test">
+                  <div :class="'slide-bg ' + backgroundValue">
+                    {{ item.title }}
+                  </div>
               </div>
           </v-row>
         </div>
       </v-col>
+      
+      <div class="square-btn d-flex flex-column ">
+        <v-btn @click="toggleTheme" class="theme-btn bg-yellow d-flex align-center justify-center pa-0">
+              <v-icon class="mr-3 mb-2">{{ iconValue }}</v-icon>
+        </v-btn>
+        <v-btn @click="toggleTheme" class="menu-btn bg-yellow d-md-none d-flex align-center justify-center pa-0">
+              <v-icon class="mr-3 mt-3 menu-btn-content">mdi-menu</v-icon>
+        </v-btn>
+      </div>
     </v-row>
   </template>
   
-  <script>
+<script>
   import { ref } from 'vue';
   import { useTheme } from 'vuetify';
 
   export default {
     setup() {
-      // Add any setup code here
-        const theme = useTheme();
-        const icon = ref(theme.global.name.value === 'light' ? 'mdi-lightbulb-off' : 'mdi-lightbulb-on');
-        
-        function toggleTheme() {
-            theme.global.name.value = theme.global.name.value === 'light' ? 'dark' : 'light'
-            icon.value = theme.global.name.value === 'light' ? 'mdi-lightbulb-on' : 'mdi-lightbulb-off';
-        }
-        return{
-            toggleTheme,
-            icon
-        }
+      const theme = useTheme();
+      return { theme }; // Return to use in computed and methods
     },
+
+    computed: {
+      iconValue() {
+        return this.theme.global.name.value === 'light' ? 'mdi-lightbulb-off' : 'mdi-lightbulb-on';
+      },
+      backgroundValue() {
+        return this.theme.global.name.value === 'light' ? 'light' : 'dark';
+      }
+    },
+
     components: {
     },
+
     data() {
       return {
-        menuItems: ['BLOGS', 'PROJECTS', 'CONTACT'],
+        menuItems: [
+          {
+            id: 0,
+            title: 'BLOGS',
+            components: null
+          }, 
+          {
+            id: 1,
+            title: 'PROJECTS',
+            components: null
+          }, 
+          {
+            id: 2,
+            title: 'CONTACT',
+            components: null
+          }
+        ],
       };
     },
+
     methods: {
       test() {
+
+      },
+
+      toggleTheme() {
+        this.theme.global.name.value = this.theme.global.name.value === 'light' ? 'dark' : 'light';
       }
     }
   };
-  </script>
+</script>
   
 <style scoped>
   /* Add any additional styles if needed */
-  .theme-change-col {
-    max-width: 100px;
+  .menu-text {
+    font-weight: bold;
   }
 
   .header-bar {
@@ -67,30 +96,24 @@
 
   .theme-btn {
     clip-path: polygon(0 0, 100% 0, 100% 70%, 0% 100%);
-  }
-
-  .square-btn {
-    width: 20%;
-    min-height: 60px;
     border-radius: 0px;
-    cursor: pointer;
+    min-height: 60px;
     font-size: 1.5rem;
   }
 
-  .normal-btn::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 100%;
-    width: 0;
-    background-color: red;
-    transition: width 0.5s ease;
-    z-index: -1;
+  .menu-btn {
+    clip-path: polygon(0 30%, 100% 0, 100% 100%, 0% 100%);
+    border-radius: 0px;
+    font-size: 1.5rem;
+    min-height: 60px;
   }
 
-  .normal-btn:hover::before {
-    width: 100%;
+  .square-btn {
+    width: 6%;
+    /* min-height:; */
+    border-radius: 0px;
+    cursor: pointer;
+    
   }
 
   @media (max-width: 1080px) {
@@ -102,42 +125,58 @@
       min-height: 80px;
     }
     .square-btn {
-      min-width: 30%;
+      min-width: 10%;
       min-height: 80px;
     }
   }
 
   @media (max-width: 600px) {
     .square-btn {
-      min-width: 35%;
+      min-width: 15%;
     }
   }
-.slide-bg {
-  position: relative;
-  z-index: 1;
-  cursor: pointer;
-  transition: color 0.2s ease;
-}
 
-.slide-bg::before {
-  content: '';
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 100%;
-  width: 0;
-  background-color: #000000;
-  transition: width 0.5s ease;
-  z-index: -1;
-}
+  .slide-bg {
+    position: relative;
+    z-index: 1;
+    cursor: pointer;
+    transition: color 0.2s ease;
+    font-weight: bold;
+  }
 
-.slide-bg:hover::before {
-  width: 100%;
-}
+  .slide-bg::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 0;
+    transition: width 0.5s ease;
+    z-index: -1;
+  }
 
-.slide-bg:hover {
-  color: white;
-}
+  .light::before {
+    background-color: #000;
+  }
 
+  .dark::before {
+    background-color: #fff;
+  }
+
+  .slide-bg:hover::before {
+    width: 100%;
+  }
+
+  .light:hover {
+    color: #fff;
+  }
+
+  .dark:hover {
+    color: #000;
+  }
+
+  .menu-btn-content {
+    transform: rotate(-6deg);
+  }
 
 </style>
